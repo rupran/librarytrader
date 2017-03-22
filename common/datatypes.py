1af9e54935d27b0f8c5f5b47413d565ce8f6a729
Andreas Ziegler <andreas.ziegler@fau.de>
2017-03-22 14:40:21 +0100

    library: more work

--- common/datatypes.py
+++ common/datatypes.py
@@ -1,7 +1,3 @@
-import os
-import re
-import sys
-
 class BaseStore:
 
     def __init__(self):
@@ -28,35 +24,6 @@ class BaseStore:
         else:
             return default
 
-
-class LDResolve(BaseStore):
-
-    def __init__(self):
-        super(LDResolve, self).__init__()
-
-        lines = os.popen('/sbin/ldconfig -p')
-        for line in lines.readlines()[1:]:
-            line = line.strip()
-            match = re.match(r'(\S+)\s+\((.+)\)\s+=>\ (.+)$', line)
-            if match:
-                libname, fullpath = match.group(1), match.group(3)
-                if libname in self:
-                    self[libname].append(fullpath)
-                else:
-                    self[libname] = [fullpath]
-            else:
-                print("WARN: ill-formed line '{}'".format(line), file=sys.stderr)
-
-    def get_paths(self, libname):
-        return self.get(libname)
-
-
-class LibraryArchive(BaseStore):
-
-    def get_library(self, path):
-        return self.get(path)
-
-    def add_library(self, path, library):
-        self[path] = library
-
-
+    def reset(self):
+        del self.storage
+        self.storage = {}
--- library.py
+++ library.py
@@ -4,10 +4,12 @@ import collections
 import os
 import sys
 
+from elftools.common.exceptions import ELFError
+from elftools.elf.dynamic import DynamicSection
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import SymbolTableSection
-from elftools.elf.dynamic import DynamicSection
-from common.datatypes import LDResolve, LibraryArchive
+
+from data.container import LDResolve, LibraryArchive
 
 
 class Library:
@@ -17,16 +19,16 @@ class Library:
             raise ValueError("{} is no absolute path".format(filename))
 
         self.fullname = filename
-        self.filename = os.path.basename(filename)
-        self.elffile = ELFFile(open(filename, 'rb'))
 
-        self.machine = self.elffile.header['e_machine']
-        self.ei_class = self.elffile.header['e_ident']['EI_CLASS']
+        self.fd = open(filename, 'rb')
+        self.elffile = ELFFile(self.fd)
+        self.elfheader = self.elffile.header
 
         self.exported_functions = None
         self.undefs = None
+
         self.needed_libs = None
-        self.libmap = {}
+        self.rpaths = None
 
     def parse_symtab(self):
         exports = collections.OrderedDict()
@@ -45,66 +47,119 @@ class Library:
                         continue
 
                     if shndx == 'SHN_UNDEF':
-                        undefs[symbol.name] = symbol
+                        undefs[symbol.name] = None
                     else:
-                        exports[symbol.name] = symbol
+                        exports[symbol.name] = None
 
         return exports, undefs
 
-    def get_needed_libs(self):
-        needed = []
+    def parse_dynamic(self):
+        needed = {}
+        rpaths = []
         for section in self.elffile.iter_sections():
             if not isinstance(section, DynamicSection):
                 continue
             for tag in section.iter_tags():
                 if tag.entry.d_tag == 'DT_NEEDED':
-                    needed.append(tag.needed)
-        return needed
+                    needed[tag.needed] = None
+                elif tag.entry.d_tag == 'DT_RPATH':
+                    #TODO: RPATH vs. RUNPATH
+                    #TODO: RPATH gets passed down
+                    rpaths = [rpath.replace("$ORIGIN",
+                                            os.path.dirname(self.fullname))
+                              for rpath in tag.rpath.split(',')]
+
+        return needed, rpaths
 
     def parse_functions(self):
         self.exported_functions, self.undefs = self.parse_symtab()
-        self.needed_libs = self.get_needed_libs()
+        self.needed_libs, self.rpaths = self.parse_dynamic()
+        del self.elffile
+        self.fd.close()
+
+    def is_compatible(self, other):
+        hdr = self.elfheader
+        o_hdr = other.elfheader
+        return hdr['e_ident']['EI_CLASS'] == o_hdr['e_ident']['EI_CLASS'] and \
+            hdr['e_machine'] == o_hdr['e_machine']
+
+    def find_compatible_libs(self, resolver, cache):
+        for needed in self.needed_libs:
+            for path in resolver.get_paths(needed, self.rpaths):
+                if path in cache:
+                    target = cache[path]
+                else:
+                    target = Library(path)
+
+                if self.is_compatible(target):
+                    self.needed_libs[needed] = target.fullname
+                    # TODO: move cache handling into receiver?
+                    if path not in cache:
+                        yield target
+                    break
 
 
 if __name__ == '__main__':
 
-    cache = LibraryArchive()
     resolver = LDResolve()
+    cache = LibraryArchive()
 
-    initial = sys.argv[1]
+    paths = []
+    for path in sys.argv[1:]:
+        if os.path.isdir(path):
+            paths.extend([os.path.join(os.path.abspath(sys.argv[1]), entry.name)
+                          for entry in os.scandir(sys.argv[1])
+                          if entry.is_file()])
+        else:
+            paths.append(path)
 
-    worklist = collections.deque()
-    worklist.append(Library(sys.argv[1]))
+    for path in paths:
+        print("Processing {}".format(path), file=sys.stderr)
 
-    while worklist:
-        current_lib = worklist.pop()
-        current_lib.parse_functions()
+        if os.path.islink(path):
+            # If we get a symlink, note in in the cache and process target
+            print("{} is a symlink to {}".format(path, os.readlink(path)),
+                  file=sys.stderr)
+            target = os.readlink(path)
+            if not os.path.isabs(target):
+                target = os.path.join(os.path.dirname(path), target)
+            cache.add_library(path, target)
+            path = target
+        elif not os.path.isfile(path):
+            continue
+        elif path in cache:
+            # skip potentially processed targets of symlinks
+            continue
 
-        for needed_lib in current_lib.needed_libs:
-            for path in resolver.get_paths(needed_lib):
-                # Check if we already processed the target and skip it
-                if path in cache:
-                    continue
+        try:
+            item = Library(path)
+        except ELFError as e:
+            print("ERR: {} => {}".format(path, e), file=sys.stderr)
+            continue
 
-                # Find the matching library, add it to the worklist
-                target = Library(path)
-                if current_lib.ei_class == target.ei_class and \
-                        current_lib.machine == target.machine:
-                    worklist.append(target)
-                    current_lib.libmap[needed_lib] = target
+        worklist = collections.deque()
+        worklist.append(item)
 
-        cache.add_library(current_lib.fullname, current_lib)
+        while worklist:
+            current_lib = worklist.pop()
+            current_lib.parse_functions()
+
+            cache.add_library(current_lib.fullname, current_lib)
+
+            needed = current_lib.find_compatible_libs(resolver, cache)
+            worklist.extend(x for x in needed)
+
 
     print(len(cache))
 
-    lib = cache[initial]
+    lib = cache[paths[0]]
     undef = lib.undefs
 
     for function in undef:
         found = False
-        for imp, imp_lib in lib.libmap.items():
-            if function in imp_lib.exported_functions:
-                print('Found {} in {}'.format(function, imp_lib.fullname))
+        for imp, imp_lib in lib.needed_libs.items():
+            if function in cache[imp_lib].exported_functions:
+                print('Found {} in {}'.format(function, imp_lib))
                 found = True
                 break
         if not found:
