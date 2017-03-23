b4c2b014dfd1b86f13c74cf2711d23629c9e7c82
Andreas Ziegler <andreas.ziegler@fau.de>
2017-03-23 19:25:27 +0100

    library: some more restructuring
    - Moved symlink checking into the library part
    - Changed behaviour of symlink checking, now using os.path.realpath()
      instead of os.readlink() => gives us the real target instead of only
      one step
    - Added an API to resolve a library by name, not only by passing a
      Library object.
    - Made some methods internal (by prefixing with '_')

--- librarytrader/container.py
+++ librarytrader/container.py
@@ -1,18 +1,38 @@
+import collections
 import os
 import re
 import sys
 
+from elftools.common.exceptions import ELFError
+
 from librarytrader.common.datatypes import BaseStore
 from librarytrader.library import Library
 
+
 class LibraryArchive(BaseStore):
 
-    def __init__(self, resolver):
+    def __init__(self):
         super(LibraryArchive, self).__init__()
-        self.resolver = resolver
+        self.resolver = LDResolve()
 
     def __getitem__(self, key):
         return self.get_library(key)
+
+    def create_library(self, path):
+        if path in self:
+            return self[path]
+
+        if os.path.islink(path):
+            target = os.path.realpath(path)
+            if target in self:
+                self.add_library(path, target)
+                return self[target]
+
+        try:
+            return Library(path)
+        except (ELFError, FileNotFoundError) as err:
+            print("ERR: {} => {}".format(path, err), file=sys.stderr)
+            return None
 
     def get_library(self, path):
         result = self.get(path)
@@ -24,13 +44,15 @@ class LibraryArchive(BaseStore):
     def add_library(self, path, library):
         self[path] = library
 
-    def find_compatible_libs(self, target, callback):
+    def _find_compatible_libs(self, target, callback):
         for needed_name in target.needed_libs:
             for path in self.resolver.get_paths(needed_name, target.rpaths):
                 if path in self:
                     needed = self[path]
                 else:
-                    needed = Library(path)
+                    needed = self.create_library(path)
+                    if not needed:
+                        continue
 
                 if target.is_compatible(needed):
                     # Enter full path in origin
@@ -43,7 +65,7 @@ class LibraryArchive(BaseStore):
                     # We found the compatible one, continue with next needed lib
                     break
 
-    def resolve_libs(self, library, callback=None):
+    def _resolve_libs(self, library, callback=None):
         # We were already here once, no need to go further
         if library.fullname in self:
             return
@@ -51,31 +73,57 @@ class LibraryArchive(BaseStore):
         library.parse_functions()
         library.release_elffile()
 
+        filename = library.fullname
         # Add ourselves before processing children
+        if os.path.islink(filename):
+            target = os.path.realpath(library.fullname)
+
+            self.add_library(filename, target)
+            library.fullname = target
+
         self.add_library(library.fullname, library)
 
-        self.find_compatible_libs(library, callback)
+        self._find_compatible_libs(library, callback)
 
     def resolve_libs_single(self, library):
-        self.resolve_libs(library)
+        self._resolve_libs(library)
+
+    def resolve_libs_single_by_path(self, path):
+        library = self.create_library(path)
+        if not library:
+            return
+        else:
+            self.resolve_libs_single(library)
 
     def resolve_libs_recursive(self, library):
-        self.resolve_libs(library, callback=self.resolve_libs_recursive)
+        self._resolve_libs(library, callback=self.resolve_libs_recursive)
+
+    def resolve_libs_recursive_by_path(self, path):
+        library = self.create_library(path)
+        if not library:
+            return
+        else:
+            self.resolve_libs_recursive(library)
 
     def resolve_functions(self, library):
-        if not library.fullname in self:
+        if library.fullname not in self:
+            #TODO: self.resolve_libs_recursive(library)?
             raise ValueError(library.fullname)
+
+        result = collections.OrderedDict()
 
         for function in library.undefs:
             found = False
-            for imp, imp_lib in library.needed_libs.items():
+            for _, imp_lib in library.needed_libs.items():
                 if function in self[imp_lib].exported_functions:
-                    print('Found {} in {}'.format(function, imp_lib))
+                    result[function] = imp_lib
                     found = True
                     break
             if not found:
                 # TODO: consider symbol versioning?
                 print('Did not find {}'.format(function))
+
+        return result
 
 
 class LDResolve(BaseStore):
--- main.py
+++ main.py
@@ -3,10 +3,7 @@
 import os
 import sys
 
-from elftools.common.exceptions import ELFError
-
-from librarytrader.library import Library
-from librarytrader.container import LibraryArchive, LDResolve
+from librarytrader.container import LibraryArchive
 
 def parse_arguments():
     paths = []
@@ -21,38 +18,18 @@ def parse_arguments():
 
 if __name__ == '__main__':
 
-    resolver = LDResolve()
-    libbox = LibraryArchive(resolver)
-
+    libbox = LibraryArchive()
     paths = parse_arguments()
 
     for path in paths:
         print("Processing {}".format(path), file=sys.stderr)
 
-        if os.path.islink(path):
-            # If we get a symlink, note in in the cache and process target
-            print("{} is a symlink to {}".format(path, os.readlink(path)),
-                  file=sys.stderr)
-            target = os.readlink(path)
-            if not os.path.isabs(target):
-                target = os.path.join(os.path.dirname(path), target)
-            libbox.add_library(path, target)
-            path = target
-        elif not os.path.isfile(path):
-            continue
-        elif path in libbox:
-            # skip potentially processed libraries
-            continue
-
-        try:
-            item = Library(path)
-        except ELFError as e:
-            print("ERR: {} => {}".format(path, e), file=sys.stderr)
-            continue
-
-        libbox.resolve_libs_recursive(item)
+        libbox.resolve_libs_recursive_by_path(path)
 
     print(len(libbox))
 
-    lib = libbox[paths[0]]
-    libbox.resolve_functions(lib)
+#    lib = libbox[paths[0]]
+#    if lib:
+#        resolved = libbox.resolve_functions(lib)
+#        for key, value in resolved.items():
+#            print("Found {} in {}".format(key, value))
