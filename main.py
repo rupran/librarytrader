dbca427f7a0e01216d45b3dd7f0a0d11f7c764d5
Andreas Ziegler <andreas.ziegler@fau.de>
2017-04-03 10:44:59 +0200

    main: restructuring into class

--- main.py
+++ main.py
@@ -24,70 +24,77 @@ import sys
 
 from librarytrader.container import LibraryStore
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Evaluate imports and ' \
-        'exports of .so libraries and ELF executables.')
-    parser.add_argument('paths', type=str, nargs='*',
-                        help='the paths to process')
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        help='verbose output')
-    parser.add_argument('--debug', action='store_true',
-                        help=argparse.SUPPRESS)
-    parser.add_argument('-l', '--load', action='store',
-                        help='JSON file to load previously exported mapping')
-    parser.add_argument('-s', '--store', action='store',
-                        help='Store calculated mapping to JSON file')
-    args = parser.parse_args()
+class Runner():
 
-    loglevel = logging.WARNING
-    if args.verbose:
-        loglevel = logging.INFO
-    if args.debug:
-        loglevel = logging.DEBUG
+    def __init__(self):
+        self.parse_arguments()
+        self.store = LibraryStore()
+        self.paths = self.get_paths()
 
-    logging.basicConfig(level=loglevel)
+    def parse_arguments(self):
+        parser = argparse.ArgumentParser(description='Evaluate imports and ' \
+            'exports of .so libraries and ELF executables.')
+        parser.add_argument('paths', type=str, nargs='*',
+                            help='the paths to process')
+        parser.add_argument('-v', '--verbose', action='store_true',
+                            help='verbose output')
+        parser.add_argument('--debug', action='store_true',
+                            help=argparse.SUPPRESS)
+        parser.add_argument('-l', '--load', action='store',
+                            help='JSON file to load previously exported mapping')
+        parser.add_argument('-s', '--store', action='store',
+                            help='Store calculated mapping to JSON file')
+        self.args = parser.parse_args()
 
-    return args
+        loglevel = logging.WARNING
+        if self.args.verbose:
+            loglevel = logging.INFO
+        if self.args.debug:
+            loglevel = logging.DEBUG
 
-def get_paths(args):
-    result = []
-    for arg in args.paths:
-        if os.path.isdir(arg):
-            result.extend([os.path.join(os.path.abspath(arg), entry.name)
-                           for entry in os.scandir(arg)
-                           if entry.is_file()])
-        else:
-            result.append(arg)
-    return result
+        logging.basicConfig(level=loglevel)
+
+    def get_paths(self):
+        result = []
+        for arg in self.args.paths:
+            if os.path.isdir(arg):
+                result.extend([os.path.join(os.path.abspath(arg), entry.name)
+                               for entry in os.scandir(arg)
+                               if entry.is_file()])
+            else:
+                result.append(arg)
+        return result
+
+    def process(self):
+        if self.args.load:
+            self.store.load(self.args.load)
+        elif not self.args.paths:
+            logging.error('Please import results and/or provide paths to analyze')
+            sys.exit(1)
+
+        logging.info('Processing %d paths in total', len(self.paths))
+
+        for path in self.paths:
+            logging.info('Processing %s', path)
+            self.store.resolve_libs_recursive_by_path(path)
+
+        logging.info('Number of entries: %d', len(self.store))
+
+        if self.args.store:
+            self.store.dump(self.args.store)
+
+    def resolve(self):
+        # Demonstration for resolving
+        lst = list(self.store.keys())
+        lib = self.store.get_from_path(lst[0])
+
+        print('Resolving functions in {}'.format(lst[0]))
+        resolved = self.store.resolve_functions(lib)
+        for key, value in resolved.items():
+            print("Found {} in {}".format(key, value))
+
 
 if __name__ == '__main__':
-
-    arguments = parse_arguments()
-    store = LibraryStore()
-
-    if arguments.load:
-        store.load(arguments.load)
-    elif not arguments.paths:
-        logging.error('Please import results and/or provide paths to analyze')
-        sys.exit(1)
-
-    paths = get_paths(arguments)
-    logging.info('Processing %d paths in total', len(paths))
-
-    for path in paths:
-        logging.info('Processing %s', path)
-        store.resolve_libs_recursive_by_path(path)
-
-    logging.info('Number of entries: %d', len(store))
-
-    if arguments.store:
-        store.dump(arguments.store)
-
-    # Demonstration for resolving
-    lst = list(store.keys())
-    lib = store.get_from_path(lst[0])
-
-    print('Resolving functions in {}'.format(lst[0]))
-    resolved = store.resolve_functions(lib)
-    for key, value in resolved.items():
-        print("Found {} in {}".format(key, value))
+    runner = Runner()
+    runner.process()
+    runner.resolve()
