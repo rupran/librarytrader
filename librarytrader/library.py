rupran/librarytrader

commit 60578d102fdd62d5ab3ff67b110b7bb5c4e5add7

library.py: add support for debuginfod
If the USE_DEBUGINFOD environment variable is set,
Library.parse_symtab() will try to query debuginfod for a
debug information file.

librarytrader/library.py
@@ -30,11 +30,13 @@ from elftools.common.utils import struct_parse
 from elftools.construct import Padding, SLInt32, Struct
 from elftools.elf.enums import ENUM_RELOC_TYPE_x64, ENUM_RELOC_TYPE_i386, \
     ENUM_RELOC_TYPE_AARCH64
+from libdebuginfod import DebugInfoD
 
 DEBUG_DIR = os.path.join(os.sep, 'usr', 'lib', 'debug')
 BUILDID_DIR = os.path.join(os.sep, DEBUG_DIR, '.build-id')
 EXTERNAL_DEBUG_DIR = os.environ.get('EXTERNAL_DEBUG_DIR')
 EXTERNAL_BUILDID_DIR = os.environ.get('EXTERNAL_BUILDID_DIR')
+USE_DEBUGINFOD = os.environ.get('USE_DEBUGINFOD')
 
 def _round_up_to_alignment(size, alignment):
     if size == 0:
@@ -788,6 +790,7 @@ class Library:
         external_elf = None
         symtab = self._elffile.get_section_by_name('.symtab')
         if not symtab:
+            logging.debug('looking for external file with .symtab')
             paths = [os.path.join(DEBUG_DIR, self.fullname[1:])]
             if EXTERNAL_DEBUG_DIR:
                 for debug_path in EXTERNAL_DEBUG_DIR.split(':'):
@@ -812,6 +815,18 @@ class Library:
                         paths.insert(0, os.path.join(EXTERNAL_BUILDID_DIR,
                                                      build_id_folder,
                                                      build_id_file))
+                    if USE_DEBUGINFOD:
+                        logging.debug('trying to query debuginfod')
+                        try:
+                            with DebugInfoD() as debuginfod:
+                                fd, path = debuginfod.find_debuginfo(build_id)
+                                if fd > 0:
+                                    os.close(fd)
+                                    paths.insert(0, path)
+                                    logging.debug('debuginfod found file at %s', path)
+                        except Exception as e:
+                            logging.debug('debuginfod query failed: %s', e)
+
             for path in paths:
                 if not os.path.isfile(path):
                     continue

setup.py
@@ -12,6 +12,7 @@ setup(
     zip_safe = False,
     install_requires = [
         'pyelftools',
-        'capstone'
+        'capstone>=4',
+        'pylibdebuginfod'
     ]
 )

commit 81f58ec9c8b3ac6f112cdcf6eee7829d04b2cd65

library.py: only query debuginfod if no local file was found
Instead of always asking debuginfod (and thus, potentially
triggering a download for every ELF file), try to find a
local file first and only ask debuginfod for a download if no
such file was found.

Fixes: 60578d102fdd ("library.py: add support for debuginfod")

librarytrader/library.py
@@ -788,6 +788,8 @@ class Library:
 
     def parse_symtab(self):
         external_elf = None
+        external_path = None
+        build_id = None
         symtab = self._elffile.get_section_by_name('.symtab')
         if not symtab:
             logging.debug('looking for external file with .symtab')
@@ -815,17 +817,6 @@ class Library:
                         paths.insert(0, os.path.join(EXTERNAL_BUILDID_DIR,
                                                      build_id_folder,
                                                      build_id_file))
-                    if USE_DEBUGINFOD:
-                        logging.debug('trying to query debuginfod')
-                        try:
-                            with DebugInfoD() as debuginfod:
-                                fd, path = debuginfod.find_debuginfo(build_id)
-                                if fd > 0:
-                                    os.close(fd)
-                                    paths.insert(0, path)
-                                    logging.debug('debuginfod found file at %s', path)
-                        except Exception as e:
-                            logging.debug('debuginfod query failed: %s', e)
 
             for path in paths:
                 if not os.path.isfile(path):
@@ -834,16 +825,31 @@ class Library:
                     external_elf = ELFFile(open(path, 'rb'))
                     symtab = external_elf.get_section_by_name('.symtab')
                     if symtab:
-                        logging.debug('Found external symtab for %s at %s',
-                                      self.fullname, path)
+                        external_path = path
                         break
                 except (ELFError, OSError) as err:
                     logging.debug('Failed to open external symbol table for %s at %s: %s',
                                   self.fullname, path, err)
                     continue
 
+        # If local paths failed, try debuginfod if requested
+        if not symtab and USE_DEBUGINFOD and build_id:
+            logging.debug('No local .symtab file, trying to query debuginfod')
+            try:
+                with DebugInfoD() as debuginfod:
+                    fd, path = debuginfod.find_debuginfo(build_id)
+                    if fd > 0:
+                        external_elf = ELFFile(os.fdopen(fd, 'rb'))
+                        symtab = external_elf.get_section_by_name('.symtab')
+                        external_path = path
+            except (ELFError, OSError, FileNotFoundError) as e:
+                logging.debug('debuginfod query failed: %s', e)
+
         if not symtab:
             return False
+        else:
+            logging.debug('Found external symtab for %s at %s', self.fullname,
+                          external_path)
 
         sorted_ranges = sorted(self.ranges.items())
         for _, symbol in self._get_function_symbols(symtab, prefix_local=True):