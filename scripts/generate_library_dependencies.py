619c1330e445dbdefe7a4bb851d5ee1a29debe55
Andreas Ziegler <andreas.ziegler@fau.de>
2022-02-18 11:55:16 +0100

    scripts: only print basename when dumping library dependencies

--- scripts/generate_library_dependencies.py
+++ scripts/generate_library_dependencies.py
@@ -13,12 +13,14 @@ seen = set()
 with open(sys.argv[1] + '_dependencies.dot', 'w') as outfd:
     outfd.write('digraph D {\n')
     for l in s.get_library_objects():
-        outfd.write('"{}" [shape=box]\n'.format(l.fullname))
+        this_base = os.path.basename(l.fullname)
+        outfd.write('"{}" [shape=box]\n'.format(this_base))
 
         for outgoing in l.needed_libs.values():
             if outgoing in seen:
                 continue
-            outfd.write('"{}" [shape=box]\n'.format(outgoing))
-            outfd.write('"{}" -> "{}"\n'.format(l.fullname, outgoing))
+            outgoing_base = os.path.basename(outgoing)
+            outfd.write('"{}" [shape=box]\n'.format(outgoing_base))
+            outfd.write('"{}" -> "{}"\n'.format(this_base, outgoing_base))
 
     outfd.write('}\n')
